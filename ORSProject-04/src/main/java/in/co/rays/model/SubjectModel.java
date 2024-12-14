21a13308411506cdf5a2b8db8640aa2028ba89a3
abeetpatel <abeetpatel@outlook.com>
2024-12-14 16:12:22 +0530

    '14-12-2024'

--- ORSProject-04/src/main/java/in/co/rays/model/SubjectModel.java
+++ ORSProject-04/src/main/java/in/co/rays/model/SubjectModel.java
@@ -1,5 +1,271 @@
 package in.co.rays.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import in.co.rays.bean.CourseBean;
+import in.co.rays.bean.SubjectBean;
+import in.co.rays.util.JDBCDataSource;
+
 public class SubjectModel {
+
+	public long nextPk() throws Exception {
+
+		long pk = 0;
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		PreparedStatement pstmt = conn.prepareStatement("select max(id) from st_subject");
+
+		ResultSet rs = pstmt.executeQuery();
+
+		while (rs.next()) {
+
+			pk = rs.getLong(1);
+
+			System.out.println("max id => " + pk);
+
+		}
+
+		JDBCDataSource.closeConnection(conn);
+
+		return pk + 1;
+
+	}
+
+	public void add(SubjectBean bean) throws Exception {
+
+		CourseModel crmodel = new CourseModel();
+		CourseBean crbean = crmodel.finedByPk(bean.getCourseId());
+		String courseName = crbean.getName();
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		PreparedStatement pstmt = conn.prepareStatement("insert into st_subject values(?,?,?,?,?,?,?,?,?)");
+
+		pstmt.setLong(1, nextPk());
+		pstmt.setString(2, bean.getName());
+		pstmt.setLong(3, bean.getCourseId());
+		pstmt.setString(4, courseName);
+		pstmt.setString(5, bean.getDescription());
+		pstmt.setString(6, bean.getCreatedBy());
+		pstmt.setString(7, bean.getModifiedBy());
+		pstmt.setTimestamp(8, bean.getCreatedDatetime());
+		pstmt.setTimestamp(9, bean.getModifiedDatetime());
+
+		int i = pstmt.executeUpdate();
+
+		JDBCDataSource.closeConnection(conn);
+
+		System.out.println("data added successfully => " + i);
+
+	}
+
+	public void update(SubjectBean bean) throws Exception {
+
+		CourseModel crmodel = new CourseModel();
+		CourseBean crbean = crmodel.finedByPk(bean.getCourseId());
+		String courseName = crbean.getName();
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		PreparedStatement pstmt = conn.prepareStatement(
+				"update st_subject set name = ?, course_id = ?, course_name = ?, description = ?, created_by = ?, modified_by = ?, created_datetime = ?, modified_datetime = ? where id = ?");
+
+		pstmt.setString(1, bean.getName());
+		pstmt.setLong(2, bean.getCourseId());
+		pstmt.setString(3, courseName);
+		pstmt.setString(4, bean.getDescription());
+		pstmt.setString(5, bean.getCreatedBy());
+		pstmt.setString(6, bean.getModifiedBy());
+		pstmt.setTimestamp(7, bean.getCreatedDatetime());
+		pstmt.setTimestamp(8, bean.getModifiedDatetime());
+		pstmt.setLong(9, bean.getId());
+
+		int i = pstmt.executeUpdate();
+
+		JDBCDataSource.closeConnection(conn);
+
+		System.out.println("data updated successfully => " + i);
+
+	}
+
+	public void delete(long id) throws Exception {
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		PreparedStatement pstmt = conn.prepareStatement("delete from st_subject where id =?");
+
+		pstmt.setLong(1, id);
+
+		int i = pstmt.executeUpdate();
+
+		JDBCDataSource.closeConnection(conn);
+
+		System.out.println("data deleted successfully => " + i);
+
+	}
+
+	public SubjectBean finedByPk(long id) throws Exception {
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		PreparedStatement pstmt = conn.prepareStatement("select * from st_subject where id = ?");
+
+		pstmt.setLong(1, id);
+
+		ResultSet rs = pstmt.executeQuery();
+
+		SubjectBean bean = null;
+
+		while (rs.next()) {
+
+			bean = new SubjectBean();
+
+			bean.setId(rs.getLong(1));
+			bean.setName(rs.getString(2));
+			bean.setCourseId(rs.getLong(3));
+			bean.setCourseName(rs.getString(4));
+			bean.setDescription(rs.getString(5));
+			bean.setCreatedBy(rs.getString(6));
+			bean.setModifiedBy(rs.getString(7));
+			bean.setCreatedDatetime(rs.getTimestamp(8));
+			bean.setModifiedDatetime(rs.getTimestamp(9));
+
+		}
+
+		JDBCDataSource.closeConnection(conn);
+
+		return bean;
+
+	}
+
+	public SubjectBean finedByName(String name) throws Exception {
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		PreparedStatement pstmt = conn.prepareStatement("select * from st_subject where name = ?");
+
+		pstmt.setString(1, name);
+
+		ResultSet rs = pstmt.executeQuery();
+
+		SubjectBean bean = null;
+
+		while (rs.next()) {
+
+			bean = new SubjectBean();
+
+			bean.setId(rs.getLong(1));
+			bean.setName(rs.getString(2));
+			bean.setCourseId(rs.getLong(3));
+			bean.setCourseName(rs.getString(4));
+			bean.setDescription(rs.getString(5));
+			bean.setCreatedBy(rs.getString(6));
+			bean.setModifiedBy(rs.getString(7));
+			bean.setCreatedDatetime(rs.getTimestamp(8));
+			bean.setModifiedDatetime(rs.getTimestamp(9));
+
+		}
+
+		JDBCDataSource.closeConnection(conn);
+
+		return bean;
+
+	}
+
+	public List search(SubjectBean bean, int pageNo, int pageSize) throws Exception {
+
+		Connection conn = JDBCDataSource.getConnection();
+
+		StringBuffer sql = new StringBuffer("select * from st_subject where 1 = 1");
+
+		if (bean != null) {
+
+			if (bean.getName() != null && bean.getName().length() > 0) {
+
+				sql.append(" and name like '" + bean.getName() + "%'");
+
+			}
+			if (bean.getDescription() != null && bean.getDescription().length() > 0) {
+
+				sql.append(" and description like '" + bean.getDescription() + "%'");
+
+			}
+			if (bean.getCreatedBy() != null && bean.getCreatedBy().length() > 0) {
+
+				sql.append(" and created_by like '" + bean.getCreatedBy() + "%'");
+
+			}
+			if (bean.getModifiedBy() != null && bean.getModifiedBy().length() > 0) {
+
+				sql.append(" and modified_by like '" + bean.getModifiedBy() + "%'");
+
+			}
+			if (bean.getCreatedDatetime() != null && bean.getCreatedDatetime().getTime() > 0) {
+
+				Timestamp ts = new Timestamp(bean.getCreatedDatetime().getTime());
+
+				String[] arr = ts.toString().split("\\.");
+
+				System.out.println("date => " + arr[0]);
+
+				sql.append(" and created_datetime like '" + arr[0] + "%'");
+
+			}
+			if (bean.getModifiedDatetime() != null && bean.getModifiedDatetime().getTime() > 0) {
+
+				Timestamp ts = new Timestamp(bean.getModifiedDatetime().getTime());
+
+				String[] arr = ts.toString().split("\\.");
+
+				System.out.println("date => " + arr[0]);
+
+				sql.append(" and modified_datetime like '" + arr[0] + "%'");
+			}
+		}
+
+		if (pageSize > 0) {
+
+			pageNo = (pageNo - 1) * pageSize;
+
+			sql.append(" limit " + pageNo + "," + pageSize);
+
+		}
+
+		System.out.println("sql => " + sql.toString());
+
+		PreparedStatement pstmt = conn.prepareStatement(sql.toString());
+
+		ResultSet rs = pstmt.executeQuery();
+
+		List list = new ArrayList();
+
+		while (rs.next()) {
+
+			bean = new SubjectBean();
+
+			bean.setId(rs.getLong(1));
+			bean.setName(rs.getString(2));
+			bean.setCourseId(rs.getLong(3));
+			bean.setCourseName(rs.getString(4));
+			bean.setDescription(rs.getString(5));
+			bean.setCreatedBy(rs.getString(6));
+			bean.setModifiedBy(rs.getString(7));
+			bean.setCreatedDatetime(rs.getTimestamp(8));
+			bean.setModifiedDatetime(rs.getTimestamp(9));
+			list.add(bean);
+
+		}
+
+		JDBCDataSource.closeConnection(conn);
+
+		return list;
+
+	}
 
 }
--- ORSProject-04/src/main/java/in/co/rays/test/TestSubjectModel.java
+++ ORSProject-04/src/main/java/in/co/rays/test/TestSubjectModel.java
@@ -1,5 +1,140 @@
 package in.co.rays.test;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import in.co.rays.bean.SubjectBean;
+import in.co.rays.model.SubjectModel;
+
 public class TestSubjectModel {
+
+	public static void main(String[] args) throws Exception {
+
+		// testAdd();
+		// testUpdate();
+		// testDelete();
+		// testFinedByPk();
+		// testFinedByName();
+		testSearch();
+
+	}
+
+	private static void testSearch() throws Exception {
+
+		SubjectBean bean = new SubjectBean();
+		SubjectModel model = new SubjectModel();
+		int pageNo = 1;
+		int pageSize = 5;
+		List list = new ArrayList();
+
+		list = model.search(bean, pageNo, pageSize);
+
+		Iterator it = list.iterator();
+
+		while (it.hasNext()) {
+
+			bean = (SubjectBean) it.next();
+
+			System.out.print(bean.getId());
+			System.out.print("\t" + bean.getName());
+			System.out.print("\t" + bean.getCourseId());
+			System.out.print("\t" + bean.getCourseName());
+			System.out.print("\t" + bean.getDescription());
+			System.out.print("\t" + bean.getCreatedBy());
+			System.out.print("\t" + bean.getModifiedBy());
+			System.out.print("\t" + bean.getCreatedDatetime());
+			System.out.println("\t" + bean.getModifiedDatetime());
+
+		}
+
+	}
+
+	private static void testFinedByName() throws Exception {
+
+		SubjectBean bean = new SubjectBean();
+		SubjectModel model = new SubjectModel();
+		String name = "collection";
+
+		bean = model.finedByName(name);
+
+		System.out.println(bean.getId());
+		System.out.println(bean.getName());
+		System.out.println(bean.getCourseId());
+		System.out.println(bean.getCourseName());
+		System.out.println(bean.getDescription());
+		System.out.println(bean.getCreatedBy());
+		System.out.println(bean.getModifiedBy());
+		System.out.println(bean.getCreatedDatetime());
+		System.out.println(bean.getModifiedDatetime());
+
+	}
+
+	private static void testFinedByPk() throws Exception {
+
+		SubjectBean bean = new SubjectBean();
+		SubjectModel model = new SubjectModel();
+		long id = 1;
+
+		bean = model.finedByPk(id);
+
+		System.out.println(bean.getId());
+		System.out.println(bean.getName());
+		System.out.println(bean.getCourseId());
+		System.out.println(bean.getCourseName());
+		System.out.println(bean.getDescription());
+		System.out.println(bean.getCreatedBy());
+		System.out.println(bean.getModifiedBy());
+		System.out.println(bean.getCreatedDatetime());
+		System.out.println(bean.getModifiedDatetime());
+
+	}
+
+	private static void testDelete() throws Exception {
+
+		long id = 3;
+
+		SubjectModel model = new SubjectModel();
+
+		model.delete(id);
+
+	}
+
+	private static void testUpdate() throws Exception {
+
+		SubjectBean bean = new SubjectBean();
+		SubjectModel model = new SubjectModel();
+
+		bean.setId(2);
+		bean.setName("OOP");
+		bean.setCourseId(1);
+		bean.setDescription("OOP");
+		bean.setCreatedBy("admin");
+		bean.setModifiedBy("admin");
+		bean.setCreatedDatetime(new Timestamp(new Date().getTime()));
+		bean.setModifiedDatetime(new Timestamp(new Date().getTime()));
+
+		model.update(bean);
+
+	}
+
+	private static void testAdd() throws Exception {
+
+		SubjectBean bean = new SubjectBean();
+		SubjectModel model = new SubjectModel();
+
+		bean.setName("collection");
+		bean.setCourseId(1);
+		bean.setDescription("collection");
+		bean.setCreatedBy("admin");
+		bean.setModifiedBy("admin");
+		bean.setCreatedDatetime(new Timestamp(new Date().getTime()));
+		bean.setModifiedDatetime(new Timestamp(new Date().getTime()));
+
+		model.add(bean);
+
+	}
 
 }
