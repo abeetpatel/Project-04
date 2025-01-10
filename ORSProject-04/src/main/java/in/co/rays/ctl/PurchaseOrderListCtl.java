67e66e4a72bc3f009ef5d5593119135a5c41648b
abeetpatel <abeetpatel@outlook.com>
2025-01-10 18:25:51 +0530

    '10-01-2025'

--- ORSProject-04/src/main/java/in/co/rays/ctl/PurchaseOrderListCtl.java
+++ ORSProject-04/src/main/java/in/co/rays/ctl/PurchaseOrderListCtl.java
@@ -1,9 +1,140 @@
 package in.co.rays.ctl;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import in.co.rays.bean.BaseBean;
+import in.co.rays.bean.PurchaseOrderBean;
+import in.co.rays.exception.ApplicationException;
+import in.co.rays.model.PurchaseOrderModel;
+import in.co.rays.util.DataUtility;
+import in.co.rays.util.PropertyReader;
+import in.co.rays.util.ServletUtility;
 
 @WebServlet(name = "/PurchaseOrderListCtl", urlPatterns = { "/ctl/PurchaseOrderListCtl" })
 public class PurchaseOrderListCtl extends BaseCtl {
+
+	@Override
+	protected void preload(HttpServletRequest request) {
+
+		PurchaseOrderModel model = new PurchaseOrderModel();
+
+		try {
+			List productList = model.list();
+			request.setAttribute("productList", productList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	protected BaseBean populateBean(HttpServletRequest request) {
+
+		PurchaseOrderBean bean = new PurchaseOrderBean();
+
+		bean.setId(DataUtility.getLong(request.getParameter("id")));
+
+		return bean;
+
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		List<PurchaseOrderBean> list = null;
+		List<PurchaseOrderBean> next = null;
+
+		int pageNo = 1;
+		int pageSize = DataUtility.getInt(PropertyReader.getValue("page.size"));
+
+		PurchaseOrderBean bean = (PurchaseOrderBean) populateBean(request);
+		PurchaseOrderModel model = new PurchaseOrderModel();
+
+		try {
+			list = model.search(bean, pageNo, pageSize);
+			next = model.search(bean, pageNo + 1, pageSize);
+			request.setAttribute("nextListSize", next.size());
+			ServletUtility.setList(list, request);
+			ServletUtility.setPageNo(pageNo, request);
+			ServletUtility.setPageSize(pageSize, request);
+			ServletUtility.forward(getView(), request, response);
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		List<PurchaseOrderBean> list = null;
+		List<PurchaseOrderBean> next = null;
+		int pageNo = DataUtility.getInt(request.getParameter("pageNo"));
+		int pageSize = DataUtility.getInt(request.getParameter("pageSize"));
+
+		pageNo = (pageNo == 0) ? 1 : pageNo;
+		pageSize = (pageSize == 0) ? DataUtility.getInt(PropertyReader.getValue("page.size")) : pageSize;
+
+		PurchaseOrderBean bean = (PurchaseOrderBean) populateBean(request);
+		PurchaseOrderModel model = new PurchaseOrderModel();
+
+		String op = request.getParameter("operation");
+		String[] ids = request.getParameterValues("ids");
+
+		try {
+
+			if (OP_SEARCH.equalsIgnoreCase(op)) {
+				pageNo = 1;
+			} else if (OP_NEXT.equalsIgnoreCase(op)) {
+				pageNo++;
+			} else if (OP_PREVIOUS.equalsIgnoreCase(op) && pageNo > 0) {
+				pageNo--;
+			} else if (OP_NEW.equalsIgnoreCase(op)) {
+				ServletUtility.redirect(ORSView.PURCHASEORDER_CTL, request, response);
+				return;
+			} else if (OP_RESET.equalsIgnoreCase(op)) {
+				ServletUtility.redirect(ORSView.PURCHASEORDER_LIST_CTL, request, response);
+				return;
+			} else if (OP_DELETE.equalsIgnoreCase(op)) {
+				pageNo = 1;
+				if (ids != null && ids.length > 0) {
+					for (String id : ids) {
+						model.delete(DataUtility.getLong(id));
+					}
+					ServletUtility.setSuccessMessage("Data Deleted Successfully", request);
+				} else {
+					ServletUtility.setErrorMessage("Select at Least One record", request);
+				}
+			}
+
+			ServletUtility.setBean(bean, request);
+			list = model.search(bean, pageNo, pageSize);
+			next = model.search(bean, pageNo + 1, pageSize);
+
+			if (!OP_DELETE.equalsIgnoreCase(op) && (list == null || list.size() > 0)) {
+
+				ServletUtility.setErrorMessage("No Record Found", request);
+
+			}
+
+			request.setAttribute("nextListSize", next.size());
+			ServletUtility.setList(list, request);
+			ServletUtility.setPageNo(pageNo, request);
+			ServletUtility.setPageSize(pageSize, request);
+			ServletUtility.forward(getView(), request, response);
+
+		} catch (Exception e) {
+		}
+
+	}
 
 	@Override
 	protected String getView() {
